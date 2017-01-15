8590be82f9de2aca01936fcdc7a73975f6738d4e
C.S. Jiang <christine.s.jiang@gmail.com>
2017-01-15 11:58:08 -0500

    info page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './BobbleTrump.png';
+import logo from './BotArmy-01.svg';
+// import logo from './BobbleTrump.png';
 import './App.css';
+import TweetEmbed from 'react-tweet-embed';
+import {Button, Grid, Row, Col } from 'react-bootstrap';
+
 
 class App extends Component {
   render() {
+
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={1} md={2} lg={2} />
+            <Col xs={10} md={8} lg={8}>
+              <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+              </div>
+              <h1 className="text-center title">Crush fake news</h1>
+              <h2 className="text-center">with even more fake news</h2>
+              <Button bsSize="large" block className="btn btn-primary">Robotify your Twitter</Button>
+              <h3 className="text-center">For the next 48 hours, turn your Twitter into a bot blasting left-fake-news</h3>
+              <p className="text-center App-intro">
+                <a className="twitter-timeline"
+                  href="https://twitter.com/TreadCrumbs/timelines/820467882391957506" data-tweet-limit="3" data-chrome="nofooter transparent noscrollbar noheader">
+                </a>
+              </p>
+              <h4 className="text-center">And hundreds more tweets like this, for the next 48 hours.</h4>
+              <h5 className="text-center">Safe. Secure. No shady stuff.</h5>
+            </Col>
+            <Col xs={1} md={2} lg={2} />
+          </Row>
+        </Grid>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Home from './Home';
+import {Router, Route, IndexRedirect, browserHistory} from 'react-router';
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render(<Router history={browserHistory}>
+      <Route path="/" component={Home} />
+      <Route path="/join" component={App} />
+    </Router>,
   document.getElementById('root')
 );
